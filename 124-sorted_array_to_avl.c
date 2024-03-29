1e97fbf2effb1b4b07b277ea92812a7390327d50
Y-admen <yassminash305@gmail.com>
2024-03-29 02:00:32 +0200

    header

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -1,17 +1,4 @@
 #include "binary_trees.h"
-/**
- * sorted_array_to_avl - Builds an AVL tree from a sorted array
- * @array: Pointer to the first element of the array to be converted
- * @size: Number of elements in the array
- * Return: Pointer to the root node of the created AVL tree, or NULL on failure
- */
-binary_tree_t *sorted_array_to_avl(int *array, size_t size)
-{
-    if (!array || size == 0)
-        return (NULL);
-
-    return (build_avl_tree(array, 0, size - 1, NULL));
-}
 
 /**
  * build_avl_tree - Recursively constructs the AVL tree
@@ -36,4 +23,17 @@ binary_tree_t *build_avl_tree(int *array, int left, int right, binary_tree_t *pa
     root->right = build_avl_tree(array, mid + 1, right, root);
 
     return (root);
+}
+/**
+ * sorted_array_to_avl - Builds an AVL tree from a sorted array
+ * @array: Pointer to the first element of the array to be converted
+ * @size: Number of elements in the array
+ * Return: Pointer to the root node of the created AVL tree, or NULL on failure
+ */
+binary_tree_t *sorted_array_to_avl(int *array, size_t size)
+{
+    if (!array || size == 0)
+        return (NULL);
+
+    return (build_avl_tree(array, 0, size - 1, NULL));
 }
--- binary_trees.h
+++ binary_trees.h
@@ -57,5 +57,7 @@ int binary_tree_is_avl(const binary_tree_t *tree);
 bst_t *bst_search(const bst_t *tree, int value);
 avl_t *avl_insert(avl_t **tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
+avl_t *array_to_avl(int *array, size_t size);
+binary_tree_t *sorted_array_to_avl(int *array, size_t size);
 
 #endif /* _BINARY_TREES_H_ */
